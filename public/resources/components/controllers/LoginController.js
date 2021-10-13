4f6149e67833ec8d5e21f806567678052111772f
gkovan <gkovan@hotmail.com>
2021-10-13 02:20:58 -0400

    ui changes to support using id token instead of access token; now cust data is coming from id token instead of the couchDB

--- public/resources/components/controllers/LoginController.js
+++ public/resources/components/controllers/LoginController.js
@@ -10,7 +10,10 @@ app.controller('LoginController', ['$scope','$location','BlueAPIService','UserIn
 
 		 BlueAPIService.loginUser($scope.payload, function (response) {
 	 			console.log("Login Result" + response)
-				UserInfoService.state.accessToken = response.data.access_token
+				//GK We don't want to propogate the access token so comment out the next line
+				//GK UserInfoService.state.accessToken = response.data.access_token
+				//GK instead we want to propogate the id token which contains more claims info in it such as email and other stuff
+				UserInfoService.state.accessToken = response.data.id_token
 				UserInfoService.state.authenticated = true;
 				$location.path('/catalog');
 
