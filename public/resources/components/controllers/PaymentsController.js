2af9c9f6fb1eefa8c3edd30fe9ae93a3724fe108
Hemankita Perabathini <phemankita@gmail.com>
2021-12-08 20:06:56 +0530

    insert info into verify
    Signed-off-by: Hemankita Perabathini <phemankita@gmail.com>

--- public/resources/components/controllers/PaymentsController.js
+++ public/resources/components/controllers/PaymentsController.js
@@ -4,7 +4,10 @@ app.controller('PaymentsController', ['$scope', '$window', '$location', 'UserInf
 
 	$scope.loggedIn = UserInfoService.state.authenticated
 
+	$scope.billingInfo = null
+
 	BlueAPIService.getBillingInfo(UserInfoService.state.otpToken, function (response) {
+		  console.log("Get request for billing info" + response)
 			var billingInfo = response.data;
 			$scope.billingInfo = billingInfo;
 		}, function (error){
@@ -12,14 +15,46 @@ app.controller('PaymentsController', ['$scope', '$window', '$location', 'UserInf
 	});
 
 	$scope.pay = function () {
-		  console.log($scope.cardNum)
-		  if ($scope.cardNum != "1111 2222 3333 4444") {
-								$window.alert("Invalid Card details, please enter the correct details");
-								$location.path("/payments");
-						}
-			else {
-        $window.alert("Payment Successful");
-				$location.path("/catalog");
+		  // console.log($scope.cardNum)
+
+			$scope.payload = {
+										    'emails': $scope.billingInfo.emails,
+										    'addresses': $scope.billingInfo.addresses,
+										    'meta': $scope.billingInfo.meta,
+										    'schemas': $scope.billingInfo.schemas,
+										    'cardno': $scope.cardNum,
+										    'name': $scope.billingInfo.name,
+										    'groups': $scope.billingInfo.groups,
+										    'active': $scope.billingInfo.active,
+										    'id': $scope.billingInfo.id,
+										    'userName': $scope.billingInfo.userName,
+										    'phoneNumbers': $scope.billingInfo.phoneNumbers
+										}
+
+			var pattern = /[0-9\s]{13,19}/;
+			var isValid = false;
+
+			if (pattern.test($scope.cardNum)) {
+				isValid = true;
 			}
+
+			if(isValid) {
+				BlueAPIService.putCCInfo(UserInfoService.state.otpToken, $scope.payload, function (response) {
+						console.log("Send request for CC" + response)
+						$scope.result = response.data
+						$scope.success = true;
+						$scope.fail = false;
+				}, function (error){
+						console.log("Request failed: " + error);
+						$scope.success = false;
+						$scope.fail = true;
+				});
+				$window.alert("Payment Successful");
+				$location.path("/catalog");
+		  } else {
+				$window.alert("Invalid Card details, please enter the correct details");
+				$location.path("/payments");
+		  }
+
 		}
 }]);
